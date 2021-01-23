b763a358e6125c7019ad1614ba729b07a2d06c90
Joonas Tiala <joonas.tiala@gmail.com>
2021-01-23 17:59:16 +0200

    Show score on preview card

--- components/ReviewPreview.tsx
+++ components/ReviewPreview.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 const ReviewPreview: React.FC<Props> = ({
-  review: { slug, title, date, tags, coverImage },
+  review: { slug, title, date, tags, coverImage, score },
 }) => {
   return (
     <article className="flex flex-col space-y-6 pb-6 bg-white shadow focus-within:shadow-lg hover:shadow-lg">
@@ -30,29 +30,37 @@ const ReviewPreview: React.FC<Props> = ({
           </a>
         </NextLink>
       </div>
-      <h3 className="leading-none font-sans font-bold text-black text-2xl text-center">
-        <Link variant="secondary" href={`/arvostelut/${slug}`}>
-          {title}
-        </Link>
-      </h3>
-      <div className="flex items-center justify-center flex-col xs:flex-row space-y-1 xs:space-y-0 xs:space-x-4">
-        <div className="flex items-center font-sans font-semibold text-sm text-gray-600 space-x-2">
-          <Icon type="calendar" />
-          <p className="sr-only">Julkaisupäivämäärä</p>
-          <Date date={date} />
+      <div className="flex flex-col xs:flex-row space-x-0 xs:space-x-6 space-y-6 xs:space-y-0 mx-6">
+        <div className="flex flex-col flex-grow space-y-2">
+          <h3 className="leading-none font-sans font-bold text-black text-2xl text-center xs:text-left">
+            <Link variant="secondary" href={`/arvostelut/${slug}`}>
+              {title}
+            </Link>
+          </h3>
+          <div className="flex flex-col xs:flex-row items-center justify-center xs:justify-start space-y-1 xs:space-y-0 space-x-0 xs:space-x-4">
+            <div className="flex items-center font-sans font-semibold text-sm text-gray-600 space-x-2">
+              <Icon type="calendar" alt="Julkaisupäivämäärä" />
+              <Date date={date} />
+            </div>
+            <div className="flex items-center font-sans font-semibold text-sm text-gray-600 space-x-2">
+              <Icon type="tag" alt="Tagit" />
+              <ul className="list-none space-x-2 flex flex-row">
+                {tags.map(({ slug, name }) => (
+                  <li key={slug}>
+                    <Link variant="tertiary" href={`/tagit/${slug}`}>
+                      {name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="flex items-center font-sans font-semibold text-sm text-gray-600 space-x-2">
-          <Icon type="tag" />
-          <p className="sr-only">Tagit</p>
-          <ul className="list-none space-x-2 flex flex-row">
-            {tags.map(({ slug, name }) => (
-              <li key={slug}>
-                <Link variant="tertiary" href={`/tagit/${slug}`}>
-                  {name}
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <div className="flex flex-col justify-center">
+          <div className="flex justify-center items-baseline font-bold text-red-800 text-4xl font-sans">
+            {score.total}
+            <span className="text-gray-500 text-2xl">/20</span>
+          </div>
         </div>
       </div>
     </article>
